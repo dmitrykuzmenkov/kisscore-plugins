907f9eae59617287f03b80804f435ca76258c58e
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-01-31 21:20:19 +0300

    Async module fixes

--- Async/Async.php
+++ Async/Async.php
@@ -1,8 +1,11 @@
 <?php
+// Still fucking buggy. Errors on static var of functions (connections);
 class Async extends Thread {
   protected $closure;
   protected $args = [];
   protected $result;
+  protected $processed = false;
+  protected $error = [];
 
   protected function __construct(Closure $closure, array $args = []) {
     $this->closure = $closure;
@@ -11,16 +14,27 @@ class Async extends Thread {
 
   public function run() {
     $closure = $this->closure;
-    $this->synchronized(function () use ($closure) {
-      $this->result = $closure(...$this->args);
-      $this->notify();
+    $core = getenv('KISS_CORE');
+    $this->synchronized(function () use ($closure, $core) {
+      try {
+        $this->result = $closure(...$this->args);
+      } catch (Exception $E) {
+        $this->error = ['message' => $E->getMessage(), 'trace' => $E->getTraceAsString()];
+      } finally {
+        $this->processed = true;
+        $this->notify();
+      }
     });
   }
 
   public function getResult() {
     return $this->synchronized(function () {
-      while (!$this->result) {
+      while (!$this->processed) {
         $this->wait();
+      }
+      if ($this->error) {
+        App::log($this->error['message'], ['trace' => $this->error['trace']]);
+        trigger_error('Error during thread process');
       }
       return $this->result;
     });
