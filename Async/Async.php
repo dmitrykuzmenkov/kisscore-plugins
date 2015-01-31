981deefbb93b84fe95f7a872587ec85375c44934
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-01-31 15:23:24 +0300

    Added Async module

---
+++ Async/Async.php
@@ -0,0 +1,34 @@
+<?php
+class Async extends Thread {
+  protected $closure;
+  protected $args = [];
+  protected $result;
+
+  private function __construct(Closure $closure, array $args = []) {
+    $this->closure = $closure;
+    $this->args    = $args;
+  }
+
+  public function run() {
+    $closure = $this->closure;
+    $this->synchronized(function () use ($closure) {
+      $this->result = $closure(...$this->args);
+      $this->notify();
+    });
+  }
+
+  public function getResult() {
+    return $this->synchronized(function () {
+      while (!$this->result) {
+        $this->wait();
+      }
+      return $this->result;
+    });
+  }
+
+  public static function exec(Closure $closure, array $args = []) {
+    $Async = new static($closure, $args);
+    $Async->start();
+    return $Async;
+  }
+}
