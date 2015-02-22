37357f36096190ab9b426197865c0ba58d4a5778
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-02-22 13:46:36 +0300

    Fix mysql bug

--- DB/configure.php
+++ DB/configure.php
@@ -2,7 +2,7 @@
 App::configure(__DIR__, [
 	'%DB_DIR%' => config('mysql.data_dir'),
 	'%HOST%'   => config('mysql.host'),
-	'%IP%'	   => config('mysql.host'),
+	'%IP%'	   => config('mysql.ip'),
 	'%PORT%'   => config('mysql.port'),
 	'%USER%'	 => config('mysql.user'),
 	'%PASSWORD%' => config('mysql.password'),
