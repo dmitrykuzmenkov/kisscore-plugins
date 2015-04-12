2f97a8b4357473a08a6c7e3a57fc2a53818be1c5
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-04-13 02:44:09 +0300

    Fix public sitemap and robots issues

--- SEO/configure.php
+++ SEO/configure.php
@@ -2,3 +2,6 @@
 Env::configure(__DIR__, [
   '%SERVER_NAME%' => config('common.domain'),
 ]);
+
+App::exec('test ! -f $HTML_DIR/robots.txt && /bin/ln -s $CONFIG_DIR/robots.txt $_');
+App::exec('test ! -f $HTML_DIR/sitemap.xml && /bin/ln -s $CONFIG_DIR/sitemap.xml $_');
