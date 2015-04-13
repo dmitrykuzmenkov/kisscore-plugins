3a8b6a8bb70e85ce84ed03833d0c17f03d766b7f
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-04-14 02:26:23 +0300

    Removed old SEO to be replaced

--- SEO/configure.php
+++
@@ -1,7 +0,0 @@
-<?php
-Env::configure(__DIR__, [
-  '%SERVER_NAME%' => config('common.domain'),
-]);
-
-App::exec('/bin/rm -f $HTML_DIR/robots.txt 2> /dev/null && /bin/ln -s $CONFIG_DIR/robots.txt $_');
-App::exec('/bin/rm -f $HTML_DIR/sitemap.xml 2> /dev/null && /bin/ln -s $CONFIG_DIR/sitemap.xml $_');
--- SEO/robots.txt.tpl
+++
@@ -1,8 +0,0 @@
-User-agent: *
-Crawl-delay: 1
-Sitemap: http://%SERVER_NAME%/sitemap.xml
-
-User-agent: Yandex
-Crawl-delay: 1
-Host: mixtoria.com
-Sitemap: http://%SERVER_NAME%/sitemap.xml
