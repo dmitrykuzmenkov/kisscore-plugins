8dfb7d304b537517189bdb4d91dc1fb987aa718c
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-04-13 03:07:12 +0300

    Fix symlinks generation

--- SEO/configure.php
+++ SEO/configure.php
@@ -3,5 +3,5 @@ Env::configure(__DIR__, [
   '%SERVER_NAME%' => config('common.domain'),
 ]);
 
-App::exec('test ! -f $HTML_DIR/robots.txt && /bin/ln -s $CONFIG_DIR/robots.txt $_');
-App::exec('test ! -f $HTML_DIR/sitemap.xml && /bin/ln -s $CONFIG_DIR/sitemap.xml $_');
+App::exec('/bin/rm -f $HTML_DIR/robots.txt 2> /dev/null && /bin/ln -s $CONFIG_DIR/robots.txt $_');
+App::exec('/bin/rm -f $HTML_DIR/sitemap.xml 2> /dev/null && /bin/ln -s $CONFIG_DIR/sitemap.xml $_');
