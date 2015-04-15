3371b6eaac4129da5d2a111e8e5aa8f8e1b30744
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-04-15 15:33:17 +0300

    Added SEO plugin

---
+++ SEO/Robots.php
@@ -0,0 +1,12 @@
+<?php namespace Plugin\SEO;
+class Robots {
+  public static function generate($file, array $lines = []) {
+    file_put_contents($file, 
+      'User-agent: *;'
+      . PHP_EOL . 'Crawl-delay: 1'
+      . PHP_EOL . 'Host: ' . config('common.domain')
+      . PHP_EOL . 'Sitemap: http://' . config('common.domain') . '/sitemap.xml'
+      . ($lines ? PHP_EOL . implode(PHP_EOL, $lines) : '')
+    );
+  }
+}
---
+++ SEO/Sitemap.php
@@ -0,0 +1,31 @@
+<?php namespace Plugin\SEO;
+class Sitemap {
+  public static function generate($file, array $locs) {
+    file_put_contents($file, '<?xml version="1.0" encoding="UTF-8"?>'
+      . PHP_EOL . '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0">'
+      . PHP_EOL . '<url>'
+      . PHP_EOL . '  <loc>http://' . config('common.domain') . '</loc>'
+      . PHP_EOL . '  <lastmod>' . date('Y-m-d') . '</lastmod>'
+      . PHP_EOL . '  <priority>1.0</priority>'
+      . PHP_EOL . '</url>'
+      . PHP_EOL . '<url>'
+      . PHP_EOL . '  <loc>http://' . config('common.domain') . '</loc>'
+      . PHP_EOL . '  <lastmod>' . date('Y-m-d') . '</lastmod>'
+      . PHP_EOL . '  <priority>1.0</priority>'
+      . PHP_EOL . '  <mobile:mobile/>'
+      . PHP_EOL . '</url>'
+      . PHP_EOL . implode('', 
+        array_map(function ($loc) {
+          return 
+            PHP_EOL . '<url>'
+            . PHP_EOL . '  <loc>' . $loc['url'] . '</loc>'
+            . PHP_EOL . '  <lastmod>' . $loc['date'] . '</lastmod>'
+            . PHP_EOL . '  <priority>' . $loc['priority'] . '</priority>'
+            . PHP_EOL . '</url>'
+          ;
+        }, $locs)
+      )
+      . PHP_EOL . '</urlset>'
+    );
+  }
+}
